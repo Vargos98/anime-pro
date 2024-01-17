7cb2e3c2e6a543047cba0a7dee2429ebda18a3da
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-01-17 17:56:40 +0530

    Updated app.js and Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>Anime<strong>-Pro</strong></h1>
+    </div>
+  )
+}
+
+export default Header
